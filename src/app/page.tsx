75fe009da782b9b67ac3a1564ea3d529e02753e6
Larry Chan <larrylaren2@gmail.com>
2024-05-23 17:22:29 +0800

    additional commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
 export default function HomePage() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-            SHUT THE FUCK UP COCKSUCKER
-        </main>
-
-    );
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center justify-center
+      bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"
+    >
+      EAT BEN CHOW
+    </main>
+  );
 }
